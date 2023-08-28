11b0af482500f35bd4ce6d6f38c6f0c0dd34340c
ahmedabdelsamee2002 <141630780+ahmedabdelsamee2002@users.noreply.github.com>
2023-08-28 19:38:04 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,52 @@
 <?php
-function isLeapYear($year) {
-    if (($year % 4 == 0 && $year % 100 != 0) || $year % 400 == 0) {
-        return true;
-    } else {
-        return false;
+class Product {
+  public $id;
+  public $name;
+  public $price;
+
+  public function __construct($id, $name, $price) {
+    $this->id = $id;
+    $this->name = $name;
+    $this->price = $price;
+  }
+
+  public static function calculateTotalPrice($products) {
+    $totalPrice = 0;
+    foreach ($products as $product) {
+      $totalPrice += $product->price;
     }
+    return $totalPrice;
+  }
+
+  public function displayInfo() {
+    echo "ID: " . $this->id . "<br>";
+    echo "Name: " . $this->name . "<br>";
+    echo "Price: " . $this->price . "<br>";
+  }
 }
 
-// Example usage
-$year = 2024; // The year to check
-if (isLeapYear($year)) {
-    echo "$year is a leap year.";
-} else {
-    echo "$year is not a leap year.";
+$product1 = new Product(1, "Product 1", 100);
+$product2 = new Product(2, "Product 2", 50);
+$product3 = new Product(3, "Product 3", 200);
+$product4 = new Product(4, "Product 4", 150);
+$product5 = new Product(5, "Product 5", 75);
+$product6 = new Product(6, "Product 6", 20);
+$product7 = new Product(7, "Product 7", 90);
+$product8 = new Product(8, "Product 8", 115);
+
+$products = [$product1, $product2, $product3, $product4, $product5, $product6, $product7, $product8];
+
+$totalPrice = Product::calculateTotalPrice($products);
+echo "Total Price: " . $totalPrice . "<br>";
+
+foreach ($products as $product) {
+  $product->displayInfo();
+  echo "<br>";
 }
+
+$numberOfProducts = count($products);
+echo "Number of Products: " . $numberOfProducts;
+
 ?>
+
+
